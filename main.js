e8c734b8b8c37646b957abfd6bed7674dd7e4cac
Elise Jones <02elise.jones02@gmail.com>
2023-02-25 12:38:50 -0700

    Will push idea into array

--- main.js
+++ main.js
@@ -3,22 +3,20 @@ var currentIdea
 
 var ideaTitle = document.querySelector("#ftitle")
 var ideaBody = document.querySelector("#fbody")
-
+var cardTitle = document.querySelector('.card-title')
+var cardBody = document.querySelector('.card-body')
+var ideaCards = document.querySelector('.parent3')
 var savedPosterButton = document.querySelector('.save-button')
 
-
 savedPosterButton.addEventListener('click', generateIdea)
-
 
 function generateIdea(event){
     
     event.preventDefault()
     currentIdea = new Idea(ideaTitle.value, ideaBody.value)
+
         ideaTitle.innerText = currentIdea.ideaTitle
-        ideaBody.innerText = currentIdea.Body
+        ideaBody.innerText = currentIdea.ideaBody
         savedIdeas.push(currentIdea)
-    }
-    
-
-
+}
     
