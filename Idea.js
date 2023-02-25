2a80ba27e3a9346135baa17053e67ea7df7b647b
Caitlin Cradick <caitlincradick@gmail.com>
2023-02-25 13:03:20 -0800

    Adding most up to date files

---
+++ Idea.js
@@ -0,0 +1,12 @@
+class Idea {
+    constructor(title, body) {
+      this.id = Date.now();
+      this.star = false;
+      this.title = title;
+      this.body = body;
+    }
+
+    updateIdea(){
+
+    }
+  }
---
+++ main.js
@@ -0,0 +1,56 @@
+var savedIdeas = []
+var currentIdea
+
+var ideaTitle = document.querySelector("#ftitle")
+var ideaBody = document.querySelector("#fbody")
+var cardTitle = document.querySelector('.card-title')
+var cardBody = document.querySelector('.card-body')
+var ideaCards = document.querySelector('.parent3')
+var savedPosterButton = document.querySelector('.save-button')
+
+savedPosterButton.addEventListener('click', generateIdea)
+
+function generateIdea(event){
+    
+    event.preventDefault()
+    currentIdea = new Idea(ideaTitle.value, ideaBody.value)
+
+        ideaTitle.innerText = currentIdea.ideaTitle
+        ideaBody.innerText = currentIdea.ideaBody
+        savedIdeas.push(currentIdea)
+  
+    ideaCards.innerHTML +=
+
+    `<div class="idea-cards">
+            <p class="card-header">
+                    <img class="star" src="assets/red-star.svg"> 
+                    <img class="X" src="assets/delete.svg">
+                </p>
+                <p class="card-title" >
+                    <strong>${ideaBody.value}</strong>
+                </p>
+                <p class="card-body">
+                    <strong>${ideaTitle.value}</strong>
+                </p>
+                <div class ="card-footer">
+                    <img class="+" src="assets/comment.svg">
+                    <div class="comment">Comment</div>
+                    </div>
+                    <div>
+     `
+}
+    
+// function clearInput() {
+//     ideaCards.value = ""
+// }
+
+
+// function clearInput(){
+//     document.getElementById("idea-form").reset();
+// }
+
+// function clearInput(target) {
+//     if(target.value = 'clear input') {
+//         target.value = "";
+//     }
+// }
